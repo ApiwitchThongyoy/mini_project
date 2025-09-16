48f5803b3a5533a232b9082c647a49ccba70e025
ApiwitchThongyoy <67026506@up.ac.th>
2025-09-16 22:25:58 +0700

    ตกแต่งหน้าและเปลี่ยนรูปโลโก้

--- src/components/Main-page/Mainpage.jsx
+++ src/components/Main-page/Mainpage.jsx
@@ -1,12 +1,17 @@
-function Mainpage (){
-    return(
-        <>
-        <h1>HANG MAN</h1>
-    <button onClick={1}>
-
-    </button>
-    </>
-    )
+function Mainpage () {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-5xl mb-10 font-bold">HANG MAN</h1>
+        <button
+          onClick={() => console.log("Play")}
+          className="px-20 py-5 bg-lime-500 text-white text-lg rounded-lg hover:bg-lime-600 transition duration-200 cursor-pointer font-bold"
+        >
+          PLAY
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Mainpage;
