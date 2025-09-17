8407e6bf4bb1506651421722072fed303dd15888
Adthep Kawe a <0931939341youn@gmail.com>
2025-09-17 14:31:02 +0700

    ทำให้กด play ได้

--- src/components/Main-page/Mainpage.jsx
+++ src/components/Main-page/Mainpage.jsx
@@ -1,10 +1,12 @@
+import { useNavigate } from 'react-router-dom';
 function Mainpage () {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
         <h1 className="text-5xl mb-10 font-bold">HANG MAN</h1>
         <button
-          onClick={() => console.log("Play")}
+          onClick={() => navigate("/Hangman")}
           className="px-20 py-5 bg-lime-500 text-white text-lg rounded-lg hover:bg-lime-600 transition duration-200 cursor-pointer font-bold"
         >
           PLAY
