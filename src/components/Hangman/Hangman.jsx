9dcb18b6b5f61ef3b0719f2b3921a27bd071c969
ApiwitchThongyoy <67026506@up.ac.th>
2025-09-18 15:33:23 +0700

    เพิ่มIcon

--- src/components/Hangman/Hangman.jsx
+++ src/components/Hangman/Hangman.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import HangmanGame from "./HangmanGame";
 import HangmanDrawing from "./HangmanDrawing";
+import { ImCross } from "react-icons/im";
 
 const words = ["apple", "pieapple", "banana", "watermelon", "melon"];
-
 
 function HangmanGameSnapshot(gameObj) {
   return Object.assign(Object.create(HangmanGame.prototype), gameObj);
@@ -25,75 +25,82 @@ export default function Hangman() {
   }
 
   return (
-    <div className="text-center font-sans">
-      {/* หัวข้อเกม */}
-      <h1 className="text-6xl font-bold mb-4">Hang man</h1>
+    <div>
+      {/* ปุ่มปิด */}
+      <button className="absolute top-4 right-20 text-6xl text-[#164C11] cursor-pointer z-50">
+        <ImCross />
+      </button>
 
-      {/* รูปแขวน */}
-      <HangmanDrawing wrong={game.wrong} />
+      <div className="text-center font-sans">
+        {/* หัวข้อเกม */}
+        <h1 className="text-6xl font-bold mb-4">Hangman</h1>
 
-      {/* คำที่ต้องทาย */}
-      <p className="text-5xl tracking-widest mt-4">{game.displayWord()}</p>
+        {/* รูปแขวน */}
+        <HangmanDrawing wrong={game.wrong} />
 
-      {/* จำนวนครั้งผิด */}
-      <p className="mt-4 text-3xl">
-        Missed : {game.wrong}/{game.maxAttempts}
-      </p>
+        {/* คำที่ต้องทาย */}
+        <p className="text-5xl tracking-widest mt-4">{game.displayWord()}</p>
 
-      {/* ปุ่มตัวอักษร */}
-      {!game.isGameOver() && (
-        <div className="max-w-xl mx-auto mt-4 grid grid-cols-7 gap-2">
-          {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => {
-            const isLastRow = letter === "V";
-            return (
-              <button
-                key={letter}
-                onClick={() => handleGuess(letter)}
-                disabled={game.guessed.has(letter)}
-                className={`w-10 h-10 flex items-center justify-center rounded-full font-bold text-black text-lg cursor-pointer
-                  ${isLastRow ? "col-start-2" : ""}
-                  ${
-                    game.guessed.has(letter)
-                      ? "bg-gray-300 cursor-not-allowed"
-                      : "hover:bg-black-600 shadow-md hover:shadow-lg transition duration-200"
-                  }`}
-              >
-                {letter}
-              </button>
-            );
-          })}
-        </div>
-      )}
+        {/* จำนวนครั้งผิด */}
+        <p className="mt-4 text-3xl">
+          Missed : {game.wrong}/{game.maxAttempts}
+        </p>
 
-      {/* ข้อความชนะ */}
-      {game.isWon() && (
-        <div className="mt-10 text-3xl">
-          <h2 className="text-green-600 text-2xl mb-2">
-             YOU WIN!! THE ANSWER IS : {game.word}
-          </h2>
-          <button
-            onClick={resetGame}
-            className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-200 cursor-pointer"
-          >
-            PLAY AGAIN
-          </button>
-        </div>
-      )}
+        {/* ปุ่มตัวอักษร */}
+        {!game.isGameOver() && (
+          <div className="max-w-xl mx-auto mt-4 grid grid-cols-7 gap-2">
+            {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => {
+              const isLastRow = letter === "V";
+              return (
+                <button
+                  key={letter}
+                  onClick={() => handleGuess(letter)}
+                  disabled={game.guessed.has(letter)}
+                  className={`w-10 h-10 flex items-center justify-center rounded-full font-bold text-black text-lg cursor-pointer
+                    ${isLastRow ? "col-start-2" : ""}
+                    ${
+                      game.guessed.has(letter)
+                        ? "bg-gray-300 cursor-not-allowed"
+                        : "hover:bg-black-600 shadow-md hover:shadow-lg transition duration-200"
+                    }`}
+                >
+                  {letter}
+                </button>
+              );
+            })}
+          </div>
+        )}
 
-      {/* ข้อความแพ้ */}
-      {game.isLost() && (
-        <div className="mt-10 text-3xl">
-          <h2 className="text-red-600 text-2xl mb-2 ">
-             YOU LOSE!! THE ANSWER IS : {game.word}
-          </h2>
-          <button
-            onClick={resetGame}
-            className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-200 cursor-pointer"
-          >
-            PLAY AGAIN
-          </button>
-        </div>
-      )}
+        {/* ข้อความชนะ */}
+        {game.isWon() && (
+          <div className="mt-10 text-3xl">
+            <h2 className="text-green-600 text-2xl mb-2">
+              YOU WIN!! THE ANSWER IS : {game.word}
+            </h2>
+            <button
+              onClick={resetGame}
+              className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition duration-200 cursor-pointer"
+            >
+              PLAY AGAIN
+            </button>
+          </div>
+        )}
+
+        {/* ข้อความแพ้ */}
+        {game.isLost() && (
+          <div className="mt-10 text-3xl">
+            <h2 className="text-red-600 text-2xl mb-2">
+              YOU LOSE!! THE ANSWER IS : {game.word}
+            </h2>
+            <button
+              onClick={resetGame}
+              className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition duration-200 cursor-pointer"
+            >
+              PLAY AGAIN
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
